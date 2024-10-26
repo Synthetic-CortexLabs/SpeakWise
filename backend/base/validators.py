db26b05232712a593a052ebddbc53bfdf12c4df5
Ezra Yendau <5201040538@st.aamusted.edu.gh>
2024-10-26 15:23:51 +0000

    implemented requested changes

--- backend/base/validators.py
+++ backend/base/validators.py
@@ -1,9 +1,17 @@
+"""Validators file."""
+
 from django.utils import timezone
 from rest_framework import serializers
 from django.utils.translation import gettext_lazy as _
 
 
 def validate_date_time_values(start_time, end_time):
+    """
+    this function validates date time values
+    from serializer data ensuring correct
+    data time values are entered.
+    """
+
     if start_time > end_time:
         raise serializers.ValidationError(_("Start date should be before end date"))
     if start_time < timezone.now():
