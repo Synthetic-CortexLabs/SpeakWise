8592c16efba75f9c47994d5e77e4e8d5a3a29ce8
Ezra Yendau <5201040538@st.aamusted.edu.gh>
2024-10-27 23:52:48 +0000

    events api endpoint with api docs

--- backend/speakwise/urls.py
+++ backend/speakwise/urls.py
@@ -14,9 +14,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularSwaggerView,
+    SpectacularAPIView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    # Installed apps urls
+    path("api/events/", include("events.urls", namespace="events")),
+    path("api/docs/", SpectacularSwaggerView.as_view()),
+    # Swagger UI api auto documentation urls :
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
