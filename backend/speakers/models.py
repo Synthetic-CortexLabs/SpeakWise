dc184e14dec1e3c3e59a756f3ecbe1fcff3ce43f
Johanan-Ayadata <johanan@workwithaya.com>
2024-10-28 02:36:12 +0000

    refactored speaker model to inherit timestamped model

--- backend/speakers/models.py
+++ backend/speakers/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from base.models import TimestampedModel
 
 
 # Create your models here.
-class Speaker(models.Model):
+class Speaker(TimestampedModel):
     user_id = models.OneToOneField(User, on_delete=models.CASCADE)
     twitter = models.CharField(max_length=50)
     organization = models.CharField(max_length=100)
     bio = models.TextField()
     avatar = models.ImageField(upload_to="speakers/avatars/")
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = "speakers"
