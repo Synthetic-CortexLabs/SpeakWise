bc48bf9227cff26525ca7b0a997badb7ae2199ef
Ezra Yendau <5201040538@st.aamusted.edu.gh>
2024-10-29 13:01:29 +0000

    fixed main urls files

--- backend/speakwise/urls.py
+++ backend/speakwise/urls.py
@@ -39,5 +39,6 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
