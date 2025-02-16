8354aed6666aa3598dbc9e30c8b19ee828d9a511
Julius Boakye <bbjulius4@gmail.com>
2025-02-16 17:24:42 +0000

    Fix syntax error in SESSION_COOKIE_NAME assignment in production settings

--- speakwise/speakwise/config/settings/production.py
+++ speakwise/speakwise/config/settings/production.py
@@ -68,7 +68,7 @@ SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
 # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
 SESSION_COOKIE_SECURE = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-name
-SESSION_COOKIE_NAME = "__Secure-sessionid"`
+SESSION_COOKIE_NAME = "__Secure-sessionid"
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
 CSRF_COOKIE_SECURE = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-name
