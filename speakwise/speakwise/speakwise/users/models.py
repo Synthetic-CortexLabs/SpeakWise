f701da44f7bcdb5a08a5a221addfb40d8e10ed7b
Julius Boakye <bbjulius4@gmail.com>
2025-05-08 20:59:59 +0000

    Update user model migrations to set default first name and correct migration dependencies

--- speakwise/speakwise/speakwise/users/models.py
+++ speakwise/speakwise/speakwise/users/models.py
@@ -15,7 +15,9 @@ class User(AbstractUser):
     """Custom user model."""
 
     id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=255, help_text="First name")
+    first_name = models.CharField(
+        max_length=255, help_text="First name", default="User"
+    )
     last_name = models.CharField(max_length=255, help_text="Last name ")
     email = models.EmailField(_("email address"), unique=True)
     username = models.CharField(max_length=255, null=True)
