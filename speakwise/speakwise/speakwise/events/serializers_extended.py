487ccd8bce5f473fcbbb0fe6ff87e190e3f37c33
Ezra Yendau <ezrayendau2000@hotmail.com>
2025-07-07 14:58:42 +0000

    removed extended serializer and tag model files

--- speakwise/speakwise/speakwise/events/serializers_extended.py
+++
@@ -1,7 +0,0 @@
-"""Extended event serializers with more detailed speaker and session information."""
-
-from rest_framework import serializers
-from .serializers import EventSerializer, SessionSerializer
-
-
-# is this really necessary? I think we can add this to the serializers file and inherit from the EventSerializer as we did here.
